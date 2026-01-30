94edb5e3731f95ade81f16cc3d19572a8f8fd045
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-01-30 09:02:17 +0300

    Time: 1 ms (80.7%), Space: 19.3 MB (36.22%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,25 +1,24 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-
+        
         def checker(val):
             curr_k = 1
             curr_val = 0
-            for i in range(len(nums)):
-                if curr_val + nums[i] > val:
-                    curr_val = nums[i]
+            for i in nums:
+                if curr_val + i > val:
+                    curr_val = i
                     curr_k += 1
                 else:
-                    curr_val += nums[i]
+                    curr_val += i
             return curr_k <= k
-            
+
         low = max(nums)
-        high = sum(nums)
+        high =  sum(nums)
 
-        while low <= high:
-            medium = low + (high - low)//2
-
-            if checker(medium):
-                high = medium - 1
+        while low < high:
+            middle = low + (high - low)//2
+            if checker(middle):
+                high = middle
             else:
-                low = medium + 1
+                low = middle + 1
         return low
