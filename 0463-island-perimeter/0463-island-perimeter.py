7fef0652db8d1203139c373b07a9e9ec8b4e45a3
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-01 22:43:45 +0300

    Time: 16 ms (98.79%), Space: 19.5 MB (51.32%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -3,21 +3,14 @@ class Solution:
         #dfs on each item
         row, column = len(grid),len(grid[0])
         direction = [(1,0),(0,1),(-1,0),(0,-1)]
-        def inBound(r,c):
-            return r >= 0 and r < row and c >= 0 and c < column
-
-        def dfs(i,j):
-            count = 0
-            for x, y in direction:
-                new_r = i + x
-                new_c = j + y
-                if inBound(new_r,new_c):
-                    if grid[new_r][new_c] == 1:
-                        count += 1
-            return 4 - count
         res = 0
         for i in range(row):
             for j in range(column):
                 if grid[i][j] == 1:
-                    res += dfs(i,j)
+                    res += 4
+                    if i > 0 and grid[i-1][j] == 1:
+                        res -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        res -= 2
+
         return res
