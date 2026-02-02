f93c1151d27130fa25aaacdc08aafd3b1ab204ca
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-02 22:33:49 +0300

    Time: 5 ms (25.83%), Space: 19.6 MB (14.58%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,20 +1,21 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        q =deque([(0,0),(0,1)])
+
+        visited = set(((0,0),(0,1)))
         graph = [[[],[]] for _ in range(n)]
-        q = deque([(0,0),(0,1)])
-        visited = set([(0,0),(0,1)])
-        ans = [-1 for _ in range(n)]
-        dist = 0
-        print(ans)
+        ans = [-1] * n
         for a, b in redEdges:
             graph[a][0].append(b)
-        for a,b in blueEdges:
+        for a, b in blueEdges:
             graph[a][1].append(b)
-
+        dist = 0
+        print(graph)
         while q:
             n = len(q)
+            print(q)
             for _ in range(n):
-                node,color = q.popleft()
+                node, color = q.popleft()
                 if ans[node] == -1:
                     ans[node] = dist
                 alternative = 1 - color
@@ -22,5 +23,7 @@ class Solution:
                     if (neigh,alternative) not in visited:
                         visited.add((neigh,alternative))
                         q.append((neigh,alternative))
+   
             dist += 1
         return ans
+                
