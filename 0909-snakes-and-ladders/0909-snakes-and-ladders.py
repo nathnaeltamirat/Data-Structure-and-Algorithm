8f81a652bfc46d6a8e335e90c393d48cb171f13e
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-02 22:50:04 +0300

    Time: 11 ms (96.03%), Space: 19.6 MB (9.66%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,37 +1,38 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        #constructing new grid
         n = len(board)
-        formatted  = [-1] * (n * n) 
-        visited = set([0])
+        fragmented = [-1] * (n * n)
         ltor = True
-        target = ((n*n)-1)
-        q = deque([0])
-        curr = 0
+        target = (n*n) - 1
+        idx = 0
         for i in range(n-1,-1,-1):
-            cols = range(n) if ltor else  range(n-1,-1,-1)
-            for j in cols:
-                if board[i][j] != -1:
-                    formatted[curr] = board[i][j] - 1
-                curr += 1
-            ltor = not  ltor
-
+            j = range(n) if ltor else range(n-1,-1,-1)
+            for k in j:
+                if board[i][k] != -1:
+                    fragmented[idx] = board[i][k] - 1
+                idx+=1
+            ltor = not ltor
+        
+        q = deque([0])
+        visited = set([0])
         dist = 1
         while q:
-            l = len(q)
-            for _ in range(l):
-                idx = q.popleft()
-                for i in range(idx + 1, min(idx + 6, (n**2 -1 )) + 1):
-                    val = formatted[i]
-                    if i ==  target or val == target:
+            t = len(q)
+            for _ in range(t):
+                curr = q.popleft()
+                for i in range(curr + 1,min(curr+7,n**n)):
+                    if i == target or  fragmented[i] == target:
                         return dist
-                    if val!= -1:
-                        if val not in visited:
-                            q.append(val)
-                            visited.add(val)
-                    else:
+                    if fragmented[i] == -1:
                         if i not in visited:
-                            q.append(i)
                             visited.add(i)
+                            q.append(i)
+                    else:
+                        if fragmented[i] not in visited:
+                            visited.add(fragmented[i])
+                            q.append(fragmented[i])
             dist += 1
-        return - 1
+                        
+        print(fragmented)
+        print(dist)
+        return -1
