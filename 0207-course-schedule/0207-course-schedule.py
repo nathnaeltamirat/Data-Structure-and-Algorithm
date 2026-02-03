669989f59bfb9c2af150521000bff2c3581b701e
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-03 11:05:25 +0300

    Time: 4 ms (65.09%), Space: 21.2 MB (18.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        indegree = defaultdict(int)
-        color = [-1] * numCourses
-        for a,b in prerequisites:
+        for a , b in prerequisites:
             graph[b].append(a)
-            indegree[a] += 1
+        
+        color = [-1] * numCourses
         def dfs(node):
-            val = True
+            temp = True
+            color[node] = 0
             for neigh in graph[node]:
-                if color[neigh] == -1:
-                    color[neigh] = 0
-                    val = val and dfs(neigh)
-                elif color[neigh] == 0:
-                    val = False
-            color[node] = 2
-            return val
+                if color[neigh] == 0:
+                    return False
+                elif color[neigh] == -1:
+                    temp = temp and dfs(neigh)
+            color[node] = 1
+            return temp
+        result = True
         for i in range(numCourses):
             if color[i] == -1:
-                if not dfs(i):
-                    return False
-        return True
+                result = result and dfs(i)
+        return result
+        
+
