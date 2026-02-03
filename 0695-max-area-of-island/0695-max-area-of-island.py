26c5853e7d8bdd4407594de2341c84f1da4b21d2
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-03 11:10:48 +0300

    Time: 24 ms (40.86%), Space: 24.5 MB (5.81%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,23 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        res = 0
-        row,column = len(grid), len(grid[0])
+        direction = [(1,0),(0,1),(-1,0),(0,-1)]
+        row, column = len(grid), len(grid[0])
         def inBound(r,c):
-            return r >= 0 and c >= 0 and r < row and c < column
-        direction = [(-1,0),(0,-1),(1,0),(0,1)]
+            return r >= 0 and r < row and c >= 0 and c < column
+        
+        visited = set()
         def dfs(i,j):
             val = 1
+            visited.add((i,j))
             for x, y in direction:
-                new_r = x + i
-                new_c = y + j
+                new_r = i + x
+                new_c = j + y
                 if inBound(new_r,new_c) and grid[new_r][new_c] == 1:
-                    grid[new_r][new_c] = 0
-                    val +=   dfs(new_r,new_c)
+                    if (new_r,new_c) not in visited:
+                        val += dfs(new_r,new_c)
             return val
+        res = 0
         for i in range(row):
             for j in range(column):
-                if grid[i][j] == 1:
-                    grid[i][j] = 0
-                    curr_max = dfs(i,j)
-                    res= max(res,curr_max) 
+                if (i,j) not in visited and grid[i][j] == 1:
+                    res = max(res,dfs(i,j))
         return res
