a55c1ebe9b1aa16bcf066049e262b570d1a87373
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-03 11:29:05 +0300

    Time: 0 ms (100%), Space: 19.6 MB (37.62%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,37 +1,35 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        elements =set()
         graph = defaultdict(list)
+        valid_values = set()
         for i in range(len(equations)):
-            a = equations[i][0]
-            b = equations[i][1]
-            value = values[i]
-            graph[a].append([b,value])
-            graph[b].append([a,1/value])
-            elements.add(a)
-            elements.add(b)
-        print(graph)
+            val = values[i]
+            a, b = equations[i]
+            graph[a].append([b,val])
+            graph[b].append([a,1/val])
+            valid_values.add(a)
+            valid_values.add(b)
+        visited = set()
         res = []
-        def dfs(a,b,visited,curr):
-            visited.add(a)
-            if a == b:
+        def dfs(source, destination):
+            visited.add(source)
+            if source == destination:
                 return 1
-
-            for neigh, c_val in graph[a]:
-                if neigh not in visited:
-                    if neigh == b:
-                        return curr * c_val
-                    val = dfs(neigh,b,visited,c_val * curr)
+            val = 1
+            for neigh,neigh_val in graph[source]:
+                if (neigh) not in visited:
+                    val  = dfs(neigh,destination)
                     if val != -1:
-                        return val
-                    
+                        return neigh_val * val
             return -1
-        for a,b in queries:
-  
-            if  a not in elements  or b not in elements:
+        for src, des in queries:
+            if src not in valid_values or des not in valid_values:
                 res.append(-1)
-
             else:
-                res.append(dfs(a,b,set(),1))
-
+                visited = set()
+                res.append(dfs(src,des))
+        print(graph)
+        print(res)
         return res
+
+
