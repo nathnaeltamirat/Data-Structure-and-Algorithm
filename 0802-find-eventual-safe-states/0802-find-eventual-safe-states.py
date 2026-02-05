7e43b7daac371def69b9bb8fc683e0c80eac84df
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-05 16:54:36 +0300

    Time: 83 ms (11.6%), Space: 25.4 MB (47.73%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,24 +1,24 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        terminal_nodes = set()
-        res = []
-        color = [-1] * len(graph)
+        dependency_graph = defaultdict(list)
+        indegree = defaultdict(int)
+        q = deque()
 
-        
-        #detetc cycle
-        def dfs(node):
-            if color[node] != -1:
-                return color[node] == 1
-            color[node] = 0
-            for neigh in graph[node]:
-                if color[neigh] == 0 or not dfs(neigh) :
-                    color[node] = 2
-                    return False
-             
-            color[node] = 1
-            return True
         for i in range(len(graph)):
-            dfs(i)
-        print(res)
-        return [i for i in range(len(color)) if color[i] == 1]
-        print(color)
+            for node in graph[i]:
+                dependency_graph[node].append(i)
+                indegree[i] += 1
+        res = [] 
+        for i in range(len(graph)):
+            if indegree[i] == 0:
+                q.append(i)
+
+         
+        while q:
+            node = q.popleft()
+            res.append(node)
+            for neigh in dependency_graph[node]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        return sorted(res)
