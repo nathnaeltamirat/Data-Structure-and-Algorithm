4ed067c13823ea7e637a1633ad63223421d3c203
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-05 21:00:08 +0300

    Time: 218 ms (9.03%), Space: 46.3 MB (46.73%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -3,35 +3,26 @@ class Solution:
         graph = defaultdict(list)
         indegree = defaultdict(int)
         q = deque()
-        dp = [0] * (n + 1)
-        for pre_course, next_course in relations:
-            nxt_time = time[next_course-1]
-            graph[pre_course].append([next_course,nxt_time])
-            indegree[next_course] += 1
-        
-        for course in range(1,n+1):
-            if indegree[course] == 0:
-                c_time = time[course-1]
-                q.append([course,c_time])
-                dp[course] = c_time
-        print(graph)
-        
-        res = 0
-        while q:
-            n = len(q)
-            max_time = 0
-            print(q)
-            #parallel course
-            for _ in range(n):
-                course, curr_time = q.popleft()
-                max_time = max(curr_time,max_time)
-                for next_course,next_time in graph[course]:
-                    dp[next_course] = max(dp[next_course],next_time + dp[course])
-                    indegree[next_course] -= 1
-                    #no prerequistes of course indegree -> 0
-                    if indegree[next_course] == 0:
-                        q.append((next_course,next_time))
 
-            res += max_time
+        for prev_course,next_course in relations:
+            graph[prev_course].append(next_course)
+            indegree[next_course] += 1
+        for i in range(1,n+1):
+            if indegree[i] == 0:
+                q.append(i)
+        dp = [-1] * (n+1)
+
+        while q:
+            length = len(q)
+            print(q)
+            for _ in range(length):
+                curr_course = q.popleft()
+                curr_time = time[curr_course-1]
+                dp[curr_course] = max(dp[curr_course],curr_time)
+                for neigh in graph[curr_course]:
+                    dp[neigh] = max(dp[neigh],dp[curr_course] + time[neigh - 1])
+                    indegree[neigh] -= 1
+                    if indegree[neigh] == 0:
+                        q.append(neigh)
         return max(dp)
-        
+
