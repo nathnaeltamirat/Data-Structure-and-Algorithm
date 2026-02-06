7cb211b02502be20cd56375a7695f1d2ce025cb5
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-06 12:42:57 +0300

    Time: 130 ms (83.69%), Space: 39.9 MB (61.23%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,31 +1,19 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
-        indegree = defaultdict(int)
-        q = deque()
-        for i in range(len(edges)):
-            if edges[i] != -1:
-                indegree[edges[i]] += 1
-        for i in range(len(edges)):
-            if indegree[i] == 0:
-                q.append(i)
-        while q:
-            node = q.popleft()
-            value = edges[node]
-            if value != -1:
-                indegree[value] -= 1
-                if indegree[value] == 0:
-                    q.append(value)
-        res = -1
         visited = [False] * len(edges)
+        res = -1
         for i in range(len(edges)):
-            if indegree[i] > 0 and  not  visited[i]:
-                count = 0
-                node = i
-                while not visited[node]:
-                    visited[node] = True
-                    count += 1
-                    node = edges[node]
-                res = max(res,count)
+            if visited[i]:
+                continue
+            step = 0
+            path_step = {}
+            node = i
+            while node != -1  and not visited[node]:
+                visited[node] = True
+                path_step[node] = step
+                step += 1
+                node = edges[node]
+                if node in path_step:
+                    res = max(res,step - path_step[node])
+                    break
         return res
-            
-            
