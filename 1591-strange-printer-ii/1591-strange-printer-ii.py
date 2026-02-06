92128c4e573ea0b86be6445305169572e1bc3dfc
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-06 12:11:38 +0300

    Time: 153 ms (63.51%), Space: 19.8 MB (18.92%) - LeetHub

--- 1591-strange-printer-ii/1591-strange-printer-ii.py
+++ 1591-strange-printer-ii/1591-strange-printer-ii.py
@@ -1,42 +1,40 @@
 class Solution:
     def isPrintable(self, targetGrid: List[List[int]]) -> bool:
-        count = defaultdict(int)
+        color = defaultdict(list)
         row, column = len(targetGrid), len(targetGrid[0])
-        color = {}
+        indegree = defaultdict(int)
+        graph = defaultdict(set)
+        all_elements = set()
         for i in range(row):
             for j in range(column):
-                val = targetGrid[i][j]
-                if val not in color:
-                    color[val] = [i,j,i,j]
+                value = targetGrid[i][j]
+                if value not in color:
+                    color[value] = [i,j,i,j]
                 else:
-                    color[val][0] = min(color[val][0],i) #top
-                    color[val][1] = max(color[val][1],j) #right
-                    color[val][2] = max(color[val][2],i) #bottom
-                    color[val][3] = min(color[val][3],j) # left
-        graph = defaultdict(set)
-        indegree = defaultdict(int)
-        for c in color:
-            top,right,bottom,left = color[c]
-            for i in range(top,bottom+1):
-                for j in range(left, right+ 1):
-                    val = targetGrid[i][j]
-                    if val != c:
-
-                        graph[c].add(val)
-        print(graph)
-        visited = {}
-        def dfs(node):
-            if node in visited:
-                return visited[node] != 1
-            visited[node] = 1
+                    color[value][0] = min(color[value][0],i)#top
+                    color[value][1] = max(color[value][1],j)#right
+                    color[value][2] = max(color[value][2],i)#bottom
+                    color[value][3] = min(color[value][3],j)#left
+                all_elements.add(value)
+        all_elements = list(all_elements)
+        for i in color:
+            top,right ,bottom,left = color[i]
+            for r in range(top,bottom+1):
+                for c in range(left,right+1):
+                    val = targetGrid[r][c]
+                    if i != val:
+                        if val not in graph[i]:
+                            graph[i].add(val)
+                            indegree[val]+=1
+        q = deque([node for node in color if indegree[node] == 0])
+        res = []
+        while q:
+            node = q.popleft()
+            res.append(node)
             for neigh in graph[node]:
-                if not dfs(neigh):
-                    return False
-            visited[node] = 2
-            return True
-        for c in color:
-            if c not in visited:
-                if not dfs(c):
-                    return False
-        return True
-        print(graph)
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        return True if len(res) == len(all_elements) else False
+        print(q)
+        
