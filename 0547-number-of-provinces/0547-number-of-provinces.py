d094a210ddaf7e5e8fda5b9096b275d8a5fb6318
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-06 14:35:04 +0300

    Time: 17 ms (11.03%), Space: 20.5 MB (27.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,19 +1,37 @@
+class UnionFind:
+    def __init__(self,size):
+        self.parent = {i:i for i in range((size))}
+        self.size = [1] * size
+    def find(self,x):
+        while  self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    def CountProvince(self):
+        count = set()
+        for i in range(len(self.size)):
+            count.add(self.find(i))
+        print(count)
+        return len(count)
+    def union(self,x,y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.size[rootx] > self.size[rooty]:
+                self.parent[rooty] = rootx
+                self.size[rootx] += self.size[rooty]
+            else:
+                self.parent[rootx] = rooty
+                self.size[rooty] += self.size[rootx]
+    def isConnected(self,x,y):
+        return self.find(x) == self.find(y)
+    
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for i in range(len(isConnected)):
-            for  j in range(len(isConnected[i])):
-                if i != j and isConnected[i][j]:
-                    graph[i].append(j)
-        res = 0
-        visited = set()
-        def dfs(node):
-            visited.add(node)
-            for neigh in graph[node]:
-                if neigh not in visited:
-                    dfs(neigh)
-        for i in range(len(isConnected)):
-            if i not in visited:
-                res += 1
-                dfs(i)
-        return res
+        n = len(isConnected)
+        dsu = UnionFind(n)
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] != 0:
+                    dsu.union(i,j)
+        return dsu.CountProvince()
