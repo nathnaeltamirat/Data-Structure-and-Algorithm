fcfa8b9a0973365862f0b335f38bfe6dbefaa755
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-09 20:03:52 +0300

    Time: 175 ms (11.7%), Space: 52.6 MB (36.42%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,16 +1,16 @@
 class UnionFind:
     def __init__(self):
-        self.parent =dict()
-        self.size = defaultdict(lambda : 1)
-    
+        self.parent = dict()
+        self.size = defaultdict(lambda: 1)
     def find(self,x):
         if x not in self.parent:
             self.parent[x] = x
             return x
-        if x == self.parent[x]:
+        if self.parent[x] == x:
             return x
         self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
+    
     def union(self,x,y):
         rootx = self.find(x)
         rooty = self.find(y)
@@ -21,25 +21,21 @@ class UnionFind:
             else:
                 self.parent[rootx] = rooty
                 self.size[rooty] += self.size[rootx]
-    
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        sorter = defaultdict(list)
         dsu = UnionFind()
-        for i,j in pairs:
-            dsu.union(i,j)
-        
-        target = defaultdict(list)
+
+        for i, (a,b) in enumerate(pairs):
+            dsu.union(a,b)
         for i in range(len(s)):
-            root  = dsu.find(i)
-            target[root].append(s[i])
-        for i in target:
-            target[i].sort(reverse = True)
+            root = dsu.find(i)
+            sorter[root].append(s[i])
+        for i in sorter:
+            sorter[i].sort(reverse = True)
         res = []
         for i in range(len(s)):
-            root  = dsu.find(i)
-            letter = target[root].pop()
-            res.append(letter)
-        print(target)
+            root = dsu.find(i)
+            res.append(sorter[root].pop())
         print(res)
         return "".join(res)
-        
