73e017d3274fbf9ba65ec945af0f1755c3127aa9
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-10 10:45:28 +0300

    Time: 410 ms (5.4%), Space: 61.7 MB (9.13%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -1,16 +1,19 @@
 class UnionFind:
     def __init__(self,n):
         self.parent = dict()
-        self.size = defaultdict(lambda: 1)
+        self.size = defaultdict(lambda:1)
         self.connected = n
+    
     def find(self,x):
         if x not in self.parent:
-            self.parent[x] = x
+            self.parent[x] = x 
             return x
         if x == self.parent[x]:
             return x
+        
         self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
+    
     def union(self,x,y):
         rootx = self.find(x)
         rooty = self.find(y)
@@ -24,31 +27,31 @@ class UnionFind:
             self.connected-=1
     def isConnected(self,x,y):
         return self.find(x) == self.find(y)
-
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
+        dsu_alice = UnionFind(n)
+        dsu_bob = UnionFind(n)
         edges.sort(reverse = True)
-        alice_dsu = UnionFind(n)
         res = 0
-        bob_dsu = UnionFind(n)
-        for t, a, b in edges:
-            if alice_dsu.isConnected(a,b) and bob_dsu.isConnected(a,b):
-                res += 1
-                continue
+        for i in range(len(edges)):
+            t,a,b = edges[i]
             if t == 3:
-                alice_dsu.union(a,b)
-                bob_dsu.union(a,b)
-            elif t == 1:
-                if alice_dsu.isConnected(a,b):
+                if dsu_alice.isConnected(a,b) and  dsu_bob.isConnected(a,b):
                     res += 1
-                    continue
-                alice_dsu.union(a,b)
+                else:
+                    dsu_alice.union(a,b)
+                    dsu_bob.union(a,b)
+            elif t == 2:
+                if dsu_bob.isConnected(a,b):
+                    res += 1
+                else:
+                    dsu_bob.union(a,b)
             else:
-                if bob_dsu.isConnected(a,b):
+                if dsu_alice.isConnected(a,b):
                     res += 1
-                    continue
-                bob_dsu.union(a,b)
+                else:
+                    dsu_alice.union(a,b)
 
-        if alice_dsu.connected != 1 or bob_dsu.connected != 1:
+        if dsu_alice.connected != 1 or dsu_bob.connected != 1:
             return -1
         return res
