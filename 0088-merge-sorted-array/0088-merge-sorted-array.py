30443dbb1ae399de8eb5296c722767b06135adc3
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-10 16:25:43 +0300

    Time: 0 ms (100%), Space: 19.6 MB (6.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,24 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if not n:
-            return nums1
-        ptr1 , ptr2, ptr3 = m-1, n-1, m + n -1
-
-        
-        while ptr1 >= 0 and  ptr2 >= 0:
-            print(ptr1,ptr2)
+        ptr1, ptr2, ptr3 = m-1, n-1, m+n-1
+        while ptr1 >= 0 and ptr2 >= 0:
             if nums1[ptr1] > nums2[ptr2]:
                 nums1[ptr3] = nums1[ptr1]
-                ptr1 -=1
+                ptr3-=1
+                ptr1-=1
             else:
                 nums1[ptr3] = nums2[ptr2]
                 ptr2-=1
-            ptr3-=1
-
-        #check if ptr2 is not 0
+                ptr3-=1
         while ptr2 >= 0:
             nums1[ptr3] = nums2[ptr2]
-            ptr3-=1
             ptr2-=1
+            ptr3-=1
         
