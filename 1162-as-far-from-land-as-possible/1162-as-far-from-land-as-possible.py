4cadf4895d6561ef1a47355d051c1294805b1c1d
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-11 19:20:25 +0300

    Time: 163 ms (35.94%), Space: 20.4 MB (25.45%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,37 +1,28 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-      #0 - water 
-      #1 - land  
-
-        visited = set()
         q = deque()
-        row, column = len(grid), len(grid[0])
-        direction = [(0,1),(0,-1),(1,0),(-1,0)]
-        res = float('-inf')
-        water = False
-        def inbound(r,c):
-            return r>=0 and r < row and c >= 0 and c < column
-
+        row, column  = len(grid), len(grid[0])
+        direction = [(1,0),(0,1),(-1,0),(0,-1)]
+        max_value = float('-inf')
+        def inBound(r,c):
+            return r >= 0 and r < row and c >= 0 and c < column
         for i in range(row):
             for j in range(column):
                 if grid[i][j] == 1:
-                    q.append((i,j))
-                    visited.add((i,j))
                     grid[i][j] = 0
+                    q.append((i,j))
                 else:
-                    water = True
-        if not q or not water:
-            return -1
-
+                    grid[i][j] = -1
+        # print(grid)
         while q:
             r, c = q.popleft()
-            for x,y in direction:
+            for x, y in direction:
                 new_r = x + r
                 new_c = y + c
-                if inbound(new_r,new_c) and (new_r,new_c) not in visited:
-                    visited.add((new_r,new_c))
+                if inBound(new_r,new_c) and grid[new_r][new_c] == -1:
+                    grid[new_r][new_c] = grid[r][c] + 1
+                    max_value = max(max_value,grid[new_r][new_c])
                     q.append((new_r,new_c))
-                    grid[new_r][new_c] = grid[r][c] + abs(new_r - r) + abs(new_c - c)
+        print(grid)
+        return max_value if max_value != float('-inf') else -1
 
-                    res = max(res,grid[new_r][new_c])
-        return res
