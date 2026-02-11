807734a746301d227d7dc4c401b091f666bd5457
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-11 18:47:20 +0300

    Time: 5 ms (25.71%), Space: 19.5 MB (52.56%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,29 +1,28 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        q =deque([(0,0),(0,1)])
-
-        visited = set(((0,0),(0,1)))
-        graph = [[[],[]] for _ in range(n)]
         ans = [-1] * n
-        for a, b in redEdges:
+        graph = [[[],[]] for _ in range(n)]
+        q = deque([(0,0),(0,1)])
+        visited = set([(0,0),(0,1)])
+
+        for a,b in redEdges:
             graph[a][0].append(b)
-        for a, b in blueEdges:
+        for a,b in blueEdges:
             graph[a][1].append(b)
+
         dist = 0
-        print(graph)
         while q:
-            n = len(q)
-            print(q)
-            for _ in range(n):
-                node, color = q.popleft()
+            length = len(q)
+            for _ in range(length):
+                node,color = q.popleft()
                 if ans[node] == -1:
                     ans[node] = dist
-                alternative = 1 - color
-                for neigh in graph[node][alternative]:
-                    if (neigh,alternative) not in visited:
-                        visited.add((neigh,alternative))
-                        q.append((neigh,alternative))
-   
+                alt = 1 - color
+                for neigh in graph[node][alt]:
+                    if (neigh,alt) not in visited:
+                        q.append((neigh,alt))
+                        visited.add((neigh,alt))
             dist += 1
+        print(graph)
+        print(ans)
         return ans
-                
