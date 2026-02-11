e4ff8f7f1fac96034e721973e4732fef4ce045d1
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-11 11:58:49 +0300

    Time: 115 ms (50.48%), Space: 18 MB (67.8%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,24 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        saver = defaultdict(int)
+  
+        curr = defaultdict(int)
         l = 0
+        max_count= 0
         res = 0
-        def checker(saver):
-            count = 0
-            max_value = 0
-            if len(saver) < 2:
-                return True
-            for values in saver.values():
-                count += values
-                max_value = max(max_value,values)
-            return count - max_value <= k
         for r in range(len(s)):
-            saver[s[r]] += 1    
-            while not checker(saver):
-                saver[s[l]] -= 1
-                if saver[s[l]] == 0:
-                    saver.pop(s[l])
-                l+=1
-            
+            curr[s[r]] += 1
+            max_count = max(max_count,curr[s[r]])
+            while (r-l+1)  - max_count > k:
+                curr[s[l]]-=1
+                l += 1
+       
             res = max(res,r-l+1)
         return res
