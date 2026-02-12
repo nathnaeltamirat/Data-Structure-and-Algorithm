6e684e95778e75c9b3313b97997bf824463bb13b
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-12 15:51:59 +0300

    Time: 7 ms (52.73%), Space: 23 MB (19.97%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,40 +1,42 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
-        direction = [(-1,-1),(1,1),(0,1),(1,0),(-1,0),(0,-1),(-1,1),(1,-1)]
+        direction = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
         row , column = len(board), len(board[0])
         def inBound(r,c):
-            return r >= 0 and c >= 0 and r < row and c <column
-
-        def bombFinder(i,j):
+            return r >= 0 and c >= 0 and r < row and c < column
+        def bombCalculator(i,j):
             count = 0
-            for x,y in direction:
+            for x, y in direction:
                 new_x = i + x
                 new_y = j + y
-                if inBound(new_x,new_y):
-                    if board[new_x][new_y] == "M":
-                        count += 1
+                if inBound(new_x,new_y) and board[new_x][new_y] == "M":
+                    count += 1
             return count
+        
         def dfs(i,j):
-            if board[i][j] == "E":
-                count = bombFinder(i,j)
-                if count:
-                    board[i][j] = str(count)
-                    return
-                else:
-                    board[i][j] = "B"
             if board[i][j] == "M":
                 board[i][j] = "X"
                 return
-            for x,y in direction:
+            if board[i][j] == "E":
+                value = bombCalculator(i,j)
+                if value:
+                    board[i][j] = str(value)
+                    return
+                else:
+                    board[i][j] = "B"
+            for x, y in direction:
                 new_x = i + x
                 new_y = j + y
-                if inBound(new_x,new_y):
-                    if board[new_x][new_y] == "E":
-                        count = bombFinder(new_x,new_y)
-                        if count:
-                            board[new_x][new_y] = str(count)
-                        else:
-                            board[new_x][new_y] = "B"
-                            dfs(new_x,new_y)
+                if inBound(new_x,new_y) and board[new_x][new_y] == "E":
+                    value = bombCalculator(new_x,new_y)
+                    if not value:
+                        board[new_x][new_y] ="B"
+                        dfs(new_x,new_y)
+                    else:
+                        board[new_x][new_y] = str(value)
         dfs(click[0],click[1])
+        print(board)
+                    
         return board
+        
+
