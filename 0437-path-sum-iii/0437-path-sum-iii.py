4b7c31faa4c657f2389bc456250e647fd7015d12
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-13 11:25:00 +0300

    Time: 4 ms (75.7%), Space: 20.7 MB (27.84%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,21 +6,21 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        prefix_sum = defaultdict(int)
-        prefix_sum[0] = 1
+        prefix = defaultdict(int)
+        prefix[0] = 1
         curr_sum = 0
         res = 0
         def traverse(root):
-            nonlocal curr_sum,res
+            nonlocal res, curr_sum
             if root:
                 curr_sum += root.val
-                res += prefix_sum[curr_sum - targetSum]
-                prefix_sum[curr_sum] += 1
-                
+               
+
+                res += prefix[curr_sum - targetSum]
+                prefix[curr_sum] += 1
                 traverse(root.left)
                 traverse(root.right)
-                prefix_sum[curr_sum] -= 1
+                prefix[curr_sum]-=1
                 curr_sum -= root.val
-                
         traverse(root)
         return res
