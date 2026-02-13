984fcea5656d1683075f191cdac58387c91d852f
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-13 16:59:01 +0300

    Time: 415 ms (5.63%), Space: 19.6 MB (61.97%) - LeetHub

--- 1591-strange-printer-ii/1591-strange-printer-ii.py
+++ 1591-strange-printer-ii/1591-strange-printer-ii.py
@@ -1,40 +1,44 @@
 class Solution:
     def isPrintable(self, targetGrid: List[List[int]]) -> bool:
-        color = defaultdict(list)
         row, column = len(targetGrid), len(targetGrid[0])
+        graph = defaultdict(list)
         indegree = defaultdict(int)
-        graph = defaultdict(set)
+        holder = defaultdict(list)
+        q = deque()
         all_elements = set()
         for i in range(row):
             for j in range(column):
-                value = targetGrid[i][j]
-                if value not in color:
-                    color[value] = [i,j,i,j]
+                val = targetGrid[i][j]
+                all_elements.add(val)
+                if val not in holder:
+                    holder[val] = [i,j,i,j] #top right bottom left
                 else:
-                    color[value][0] = min(color[value][0],i)#top
-                    color[value][1] = max(color[value][1],j)#right
-                    color[value][2] = max(color[value][2],i)#bottom
-                    color[value][3] = min(color[value][3],j)#left
-                all_elements.add(value)
+                    holder[val][0] = min(i,holder[val][0]) #top
+                    holder[val][1] = max(j,holder[val][1]) #right
+                    holder[val][2] = max(i,holder[val][2]) #bottom
+                    holder[val][3] = min(j,holder[val][3]) #left
+        for node in holder:
+            top,right,bottom,left = holder[node]
+            for i in range(top,bottom+1):
+                for j in range(left,right+1):
+                    val = targetGrid[i][j]
+                    if val != node and val not in graph[node]:
+                        graph[node].append(val)
+                        indegree[val] += 1
+        
         all_elements = list(all_elements)
-        for i in color:
-            top,right ,bottom,left = color[i]
-            for r in range(top,bottom+1):
-                for c in range(left,right+1):
-                    val = targetGrid[r][c]
-                    if i != val:
-                        if val not in graph[i]:
-                            graph[i].add(val)
-                            indegree[val]+=1
-        q = deque([node for node in color if indegree[node] == 0])
-        res = []
+        for i in all_elements:
+            if indegree[i] == 0:
+                q.append(i)
+            
+        order = []
         while q:
             node = q.popleft()
-            res.append(node)
+            order.append(node)
             for neigh in graph[node]:
-                indegree[neigh] -= 1
+                indegree[neigh] -=1
                 if indegree[neigh] == 0:
                     q.append(neigh)
-        return True if len(res) == len(all_elements) else False
-        print(q)
+        return True if len(order) == len(all_elements) else False
+
         
