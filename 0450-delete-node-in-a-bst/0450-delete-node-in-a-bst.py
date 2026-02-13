5d20736afd48bbec347a8c1216cfcbd7785983a6
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-13 12:06:18 +0300

    Time: 0 ms (100%), Space: 22 MB (45.31%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def inorderSuccesor(root):
+        def inOrderSuccessor(root):
             while root.left:
                 root = root.left
             return root
@@ -15,14 +15,14 @@ class Solution:
         if root.val > key:
             root.left = self.deleteNode(root.left,key)
         elif root.val < key:
-           root.right = self.deleteNode(root.right,key)
+            root.right = self.deleteNode(root.right,key)
         else:
             if not root.left:
                 return root.right
             if not root.right:
                 return root.left
             
-            temp = inorderSuccesor(root.right)
+            temp = inOrderSuccessor(root.right)
             root.val = temp.val
             root.right = self.deleteNode(root.right,temp.val)
         return root
