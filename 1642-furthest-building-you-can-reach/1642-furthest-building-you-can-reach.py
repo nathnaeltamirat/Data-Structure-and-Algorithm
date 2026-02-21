90af9c881acab6fab29f2bccb5c123e9a907a9c0
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-21 12:30:13 +0300

    Time: 99 ms (8.58%), Space: 34.5 MB (6.1%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,34 +1,17 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        def checker(building):
-            c_ladder = ladders
-            c_bricks = bricks
-            order = []
-            for i in range(1,building+1):
-                if heights[i] > heights[i-1]:
-                    order.append((heights[i] - heights[i-1]))
-            order.sort(reverse = True)
-            for i in range(len(order)):
-                if c_ladder:
-                    order[i] = 0
-                    c_ladder -= 1
-                if not c_ladder and c_bricks:
-                    if c_bricks < order[i]:
-                        return False
-                    c_bricks -= order[i]
-                    order[i] = 0
+        order_heap = []
+        for i in range(1,len(heights)):
+            if heights[i] > heights[i-1]:
+                diff = heights[i] - heights[i-1]
+                heappush(order_heap,(diff,i))
+                if len(order_heap) > ladders:
+                    if bricks >= order_heap[0][0]:
+                        bricks-= order_heap[0][0]
+                        heappop(order_heap)
+                    else:
+                        return i -1
+                    
+        return len(heights)-1
+
                 
-            return max(order) == 0 if order else True
-
-
-
-        low = 0
-        high = len(heights) - 1
-        while low <= high:
-            middle = low + (high - low)//2
-            print(middle)
-            if checker(middle):
-                low = middle + 1
-            else:
-                high = middle - 1
-        return high
