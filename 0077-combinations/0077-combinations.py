0a00d0487345d5388e7cc05bcde56c6f4a3e92e9
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-25 15:17:02 +0300

    Time: 107 ms (72.43%), Space: 61.7 MB (11.31%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        #1 --- 4
-         # 2 ---- 4
         res = []
         def backtrack(curr,path):
-            nonlocal res
+
             if len(path) == k:
-                print(path)
                 res.append(path[:])
                 return
-            max_pos = n - (k  - len(path)) + 1
-            for i in range(curr, max_pos+1):
+            for i in range(curr,n+1):
                 path.append(i)
                 backtrack(i+1,path)
                 path.pop()
+
         backtrack(1,[])
         return res
