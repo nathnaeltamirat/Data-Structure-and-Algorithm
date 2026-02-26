100c1c6f606f8141a947ec4861ec0661e7bb926b
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-02-26 14:15:05 +0300

    Time: 4 ms (6.05%), Space: 19.4 MB (63.43%) - LeetHub

--- 0306-additive-number/0306-additive-number.py
+++ 0306-additive-number/0306-additive-number.py
@@ -1,29 +1,19 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        if len(num) < 3:
-            return False
-
-        def checker(arr):
-            for i in range(2,len(arr)):
-                if arr[i-1] + arr[i-2] != arr[i]:
-                    return False
-            return True
-        self.res = False
-        def backtrack(curr,path):
-
-            if len(path) >= 3 and  path[-1] != path[-2] + path[-3]:
+        res = [False]
+        def backtrack(path,curr):
+            if len(path) >= 3 and path[-1] != path[-2] + path[-3]:
                 return False
-            if curr == len(num):
+            if len(num) == curr:
                 return len(path) >= 3
+            
             for i in range(curr,len(num)):
                 if num[curr] == "0" and i > curr:
                     break
                 path.append(int(num[curr:i+1]))
-                self.res = self.res or backtrack(i+1,path)
-                print(path)
-                if self.res:
+                res[0] = res[0] or backtrack(path,i+1)
+                if res[0]:
                     return True
                 path.pop()
-            return False
-        backtrack(0,[])
-        return self.res
+            return res[0]
+        return backtrack([],0)            
