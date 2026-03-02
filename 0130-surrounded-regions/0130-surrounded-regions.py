2db533e50f37098ccf210437f842c7f01c93d3f2
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-03-02 21:00:09 +0300

    Time: 11 ms (25.18%), Space: 22.3 MB (70.12%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,10 +3,13 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        row , column = len(board), len(board[0])
+
+        row, col = len(board), len(board[0])
+        def inBound(r,c):
+            return r >= 0 and c >= 0 and r < row and c < col
+        
         direction = [(1,0),(0,1),(-1,0),(0,-1)]
-        def inBound(x,y):
-            return x >= 0 and x < row and y >= 0 and y < column
+
         def dfs(i,j):
             for x, y in direction:
                 new_x = x + i
@@ -14,17 +17,19 @@ class Solution:
                 if inBound(new_x,new_y) and board[new_x][new_y] == "O":
                     board[new_x][new_y] = "T"
                     dfs(new_x,new_y)
-        for i in range(row):
-            for j in range(column):
-                if i == 0 or j == 0  or i == row - 1 or j == column -1:
-                    if board[i][j] == 'O':
-                        board[i][j] = "T"
-                        dfs(i,j)
+                
                 
         for i in range(row):
-            for j in range(column):
+            for j in range(col):
+                if i == 0 or j == 0 or  i == row - 1 or j == col -1:
+                    if board[i][j] == "O":
+                        board[i][j] = "T"
+                        dfs(i,j)
+        for i in range(row):
+            for j in range(col):
                 if board[i][j] == "O":
                     board[i][j] = "X"
                 elif board[i][j] == "T":
                     board[i][j] = "O"
         print(board)
+        
