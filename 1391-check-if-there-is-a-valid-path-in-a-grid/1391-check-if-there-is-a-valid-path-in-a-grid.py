4f7a4f19c313d0cf939f68b0e9cc5050c9f2db70
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-13 14:25:02 +0300

    Time: 1249 ms (5.27%), Space: 42.1 MB (39.85%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,55 +1,48 @@
 class UnionFind:
     def __init__(self):
-        self.parent = dict()
-        self.size = defaultdict(lambda: 1)
+        self.root = dict()
+        self.size = defaultdict(lambda:1)
     def find(self,x):
-        if x not in self.parent:
-            self.parent[x] = x
+        if x not in self.root:
+            self.root[x] = x
             return x
-        if x == self.parent[x]:
-            return x
-        self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
     def union(self,x,y):
-        rootx = self.find(x)
-        rooty = self.find(y)
+        rootx, rooty = self.find(x), self.find(y)
         if rootx != rooty:
             if self.size[rootx] > self.size[rooty]:
-                self.parent[rooty] = rootx
                 self.size[rootx] += self.size[rooty]
+                self.root[rooty] = rootx
             else:
-                self.parent[rootx] = rooty
-                self.size[rooty] += self.size[rootx]
+                self.root[rootx] = rooty
+                self.size[rooty] += self.size[rootx] 
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
+        #flattening formula
+ 
         dsu = UnionFind()
-        direction = {
-            1:[(0,-1),(0,1)],
-            2:[(-1,0),(1,0)],
-            3:[(0,-1),(1,0)],
-            4:[(0,1),(1,0)],
-            5:[(0,-1),(-1,0)],
-            6:[(0,1),(-1,0)]
+        dir = {
+            1: [(0,-1),(0,1)], #left and right
+            2: [(-1,0),(1,0)], #upper and lower
+            3: [(0,-1), (1,0)], #left and lower
+            4: [(0,1),(1,0)], #right and lower
+            5: [(0,-1),(-1,0)], #left and upeer
+            6: [(0,1),(-1,0)] #right and upper
         }
-        row, column = len(grid),len(grid[0])
+        row, column = len(grid), len(grid[0])
+
         def inBound(r,c):
-            return r >= 0 and r < row and c >= 0 and c < column
-        
+            return r >= 0 and c >= 0 and r < row and c < column
+
         for i in range(row):
             for j in range(column):
                 val = grid[i][j]
-                for x, y in direction[val]:
-                    new_x = x + i
-                    new_y = y + j
-                    if inBound(new_x,new_y):
-                        new_grid = grid[new_x][new_y]
-                        if (-x,-y) in direction[new_grid]:
-                            dsu.union(i * column + j,new_x * column + new_y)
-        return dsu.find(row * column - 1) == dsu.find(0)
-        # (row - 1) * (column - 1) + (column - 1)
-        # row*Column -row 
-        # row(column - 1)
-        r = m
-        c = n
-        m-1 * c + c-1
-        cm - c + c - 1
+                for x, y in dir[val]:
+                    new_r = i + x
+                    new_c = j + y
+                    if inBound(new_r,new_c):
+                        if (-x,-y) in dir[grid[new_r][new_c]]:
+                            dsu.union((i * column + j),(new_r * column  + new_c))
+        return dsu.find((row -1) * (column) + (column - 1) )== dsu.find(0)
