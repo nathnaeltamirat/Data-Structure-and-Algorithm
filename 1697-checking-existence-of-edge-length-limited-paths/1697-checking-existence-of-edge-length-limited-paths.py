a406245bf0f85b824bf56208e9a122346b6fb257
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-13 17:21:41 +0300

    Time: 481 ms (5.36%), Space: 61.3 MB (45.74%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
@@ -1,42 +1,38 @@
 class UnionFind:
     def __init__(self):
-        self.parent = dict()
+        self.root = dict()
         self.size = defaultdict(lambda:1)
-    
     def find(self,x):
-        if x not in self.parent:
-            self.parent[x] = x
+        if x not in self.root:
+            self.root[x] = x
             return x
-        if x == self.parent[x]:
-            return x
-        self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
     def union(self,x,y):
         rootx = self.find(x)
         rooty = self.find(y)
         if rootx != rooty:
             if self.size[rootx] > self.size[rooty]:
-                self.parent[rooty] = rootx
-                self.size[rootx]+= self.size[rooty]
+                self.root[rooty] = rootx
+                self.size[rootx] += self.size[rooty]
             else:
-                self.parent[rootx] = rooty
+                self.root[rootx] = rooty
                 self.size[rooty] += self.size[rootx]
     def isConnected(self,x,y):
         return self.find(x) == self.find(y)
 class Solution:
     def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
         dsu = UnionFind()
-        idx_with_queries = [ [idx,a,b,dist] for idx, (a,b,dist) in enumerate(queries)]
         edgeList.sort(key = lambda x: x[2])
-        idx_with_queries.sort(key = lambda x: x[3]) 
-        res = [0] * len(queries)
+        queries = sorted((val,a,b,idx) for idx,(a,b,val) in enumerate(queries))
+        ans = [0] * len(queries)
+        curr_e = 0
+        for i in range(len(queries)):
+            while not dsu.isConnected(queries[i][1],queries[i][2]) and curr_e < len(edgeList) and edgeList[curr_e][2] < queries[i][0]:
+                dsu.union(edgeList[curr_e][0],edgeList[curr_e][1])
+                curr_e += 1
+            ans[queries[i][3]] = dsu.isConnected(queries[i][1],queries[i][2])
+        return ans
 
 
-        e = 0
-        for idx,a,b,dist in idx_with_queries:
-            while e < len(edgeList) and  edgeList[e][2] < dist:
-                dsu.union(edgeList[e][0],edgeList[e][1])
-                e+=1
-            res[idx] = dsu.isConnected(a,b)
-        return res
-             
