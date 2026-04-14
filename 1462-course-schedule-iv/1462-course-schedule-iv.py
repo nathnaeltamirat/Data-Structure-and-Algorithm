e7a0385a93d472ff3c610bc8df6b4855fdadacea
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-14 17:32:42 +0300

    Time: 35 ms (65.14%), Space: 22.3 MB (63.6%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -2,13 +2,11 @@ class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         graph = defaultdict(list)
         indegree = defaultdict(int)
-        res = [False] * len(queries)
-        q = deque()
-        holder = defaultdict(set)
-
-        for a, b in prerequisites:
+        for a,b in prerequisites:
             graph[a].append(b)
             indegree[b] += 1
+        q = deque()
+        res = defaultdict(set)
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
@@ -16,14 +14,17 @@ class Solution:
             node = q.popleft()
             for neigh in graph[node]:
                 indegree[neigh] -= 1
-                holder[neigh].update(holder[node])
-                holder[neigh].add(node)
+                res[neigh].update(res[node])
+                res[neigh].add(node)
                 if indegree[neigh] == 0:
+
                     q.append(neigh)
-        for i in range(len(queries)):
-            a, b = queries[i]
-            if a in holder[b]:
-                res[i] = True
-            else:
-                res[i] = False
-        return res
+        
+        ans = []
+        for a,b in queries:
+            if a in res[b]:
+                ans.append(True)
+                continue
+            ans.append(False)
+   
+        return ans
