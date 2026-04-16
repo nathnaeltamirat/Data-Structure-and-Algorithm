6954b31c93b9727cf399fb6ffa7b096f71ed8e92
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-16 19:42:28 +0300

    Time: 161 ms (22.59%), Space: 46.3 MB (41.41%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -2,27 +2,23 @@ class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         graph = defaultdict(list)
         indegree = defaultdict(int)
+        for a, b in relations:
+            graph[a].append(b)
+            indegree[b] += 1
         q = deque()
-
-        for prev_course,next_course in relations:
-            graph[prev_course].append(next_course)
-            indegree[next_course] += 1
         for i in range(1,n+1):
             if indegree[i] == 0:
                 q.append(i)
         dp = [-1] * (n+1)
 
         while q:
-            length = len(q)
-            print(q)
-            for _ in range(length):
-                curr_course = q.popleft()
-                curr_time = time[curr_course-1]
-                dp[curr_course] = max(dp[curr_course],curr_time)
-                for neigh in graph[curr_course]:
-                    dp[neigh] = max(dp[neigh],dp[curr_course] + time[neigh - 1])
-                    indegree[neigh] -= 1
-                    if indegree[neigh] == 0:
-                        q.append(neigh)
+            node = q.popleft()
+            curr_time = time[node-1]
+            dp[node] = max(dp[node],curr_time)
+            for neigh in graph[node]:
+                neigh_time = time[neigh-1]
+                dp[neigh] = max(dp[neigh],neigh_time + dp[node])
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
         return max(dp)
-
