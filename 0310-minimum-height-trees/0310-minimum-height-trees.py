2516544d1590e389de67265182f03717f362e86f
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-16 19:34:21 +0300

    Time: 58 ms (39.33%), Space: 28.5 MB (47.18%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,36 +1,27 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        q = deque()
+        if n == 1:
+            return [0]
         graph = defaultdict(list)
-        indegree = defaultdict(int)
-
-        if not edges:
-            res = []
-            for i in range(n):
-                res.append(i)
-            return res
+        connection = defaultdict(int)
+        q = deque()
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
-            indegree[a] += 1
-            indegree[b] += 1
-        
+            connection[a] += 1
+            connection[b] += 1
         for i in range(n):
-            if indegree[i] == 1:
+            if connection[i] == 1:
                 q.append(i)
-        size = n
-        while q:
+        current = n
+        print(q)
+        while q and  current > 2:
             length = len(q)
-            
-            if size <= 2:
-                break
             for _ in range(length):
                 node = q.popleft()
+                current -= 1
                 for neigh in graph[node]:
-                    indegree[neigh] -= 1
-                    if indegree[neigh] == 1:
-
+                    connection[neigh] -= 1
+                    if connection[neigh] == 1:
                         q.append(neigh)
-            size -= length
-        print(q)
         return list(q)
