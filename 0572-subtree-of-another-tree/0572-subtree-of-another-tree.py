a6c8ccffe5b25bcd5decab5b20596be74f0379f2
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-22 14:11:43 +0300

    Time: 39 ms (70.04%), Space: 19.5 MB (65.91%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,29 +6,21 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        res = False
-        def isSubRoot(root,sRoot):
-            val = True
-            if not root and sRoot or (not sRoot and root):
-                return False
-            if not root and not sRoot:
+        def checker(c_root,s_root):
+            if not c_root and not s_root:
                 return True
-            if root.val != sRoot.val:
+            if not c_root or not s_root:
                 return False
-            val = val and isSubRoot(root.left,sRoot.left)
-            val = val and isSubRoot(root.right,sRoot.right)
 
-            return val
-        
-        def traverse(root):
+            if c_root.val != s_root.val:
+                return False
+            return checker(c_root.left,s_root.left) and checker(c_root.right,s_root.right)
+        res = False
+        def dfs(root,subRoot):
             nonlocal res
             if root:
-                res = res or isSubRoot(root,subRoot)
-                traverse(root.left)
-                traverse(root.right)
-        traverse(root)
+                res = res or checker(root,subRoot)
+                dfs(root.left,subRoot)
+                dfs(root.right,subRoot)
+        dfs(root,subRoot)
         return res
-        
-            
-
-        
