c1593eeb39f7cc4fd5005859c218f3707591e326
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-04-28 19:15:14 +0300

    Time: 131 ms (20.55%), Space: 46.7 MB (14.07%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -10,18 +10,18 @@ class Solution:
         def traverse(root):
             nonlocal res
             if not root:
-                return [True,0,float('inf'),float('-inf')]
-            l_bst,l_sum,l_min,l_max = traverse(root.left)
-            r_bst,r_sum, r_min,r_max = traverse(root.right)
+                return [True, 0 , float('inf'), float('-inf')]
+            l_bst, l_sum , l_min, l_max = traverse(root.left)
+            r_bst, r_sum, r_min, r_max = traverse(root.right)
 
-            if l_bst and r_bst and  l_max < root.val < r_min:
+            if l_bst and r_bst and l_max < root.val < r_min:
                 curr_sum = root.val + l_sum + r_sum
                 res = max(res,curr_sum)
-                return[
+                return [
                     True,
                     curr_sum,
-                    min(l_min,root.val),
-                    max(r_max,root.val)
+                    min(root.val,l_min),
+                    max(root.val,r_max)
                 ]
             return [
                 False,
