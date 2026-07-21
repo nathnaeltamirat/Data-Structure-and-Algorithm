510c61f70ef6b39d8992ad92e7d0cb91069f28ea
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-21 20:45:00 +0300

    Time: 1 ms (50.94%), Space: 20.9 MB (30.46%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        res = [float('-inf')]
+        res = float('-inf')
         def traverse(root,max_value,min_value):
-            res[0] = max(res[0],(max_value-min_value))
+            nonlocal res
+            res = max(res,max_value-min_value)
             if root.left:
-                traverse(root.left,max(root.left.val,max_value),min(root.left.val,min_value))
+                traverse(root.left,max(root.left.val,max_value),min(min_value,root.left.val))
             if root.right:
-                traverse(root.right,max(root.right.val,max_value),min(root.right.val,min_value))
+                traverse(root.right,max(root.right.val,max_value),min(min_value,root.right.val))
         traverse(root,root.val,root.val)
-        return res[0]
-                
+        return res
