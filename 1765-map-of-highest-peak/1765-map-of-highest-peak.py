cb169949869b2669f86f3ec553894c816e2f1c36
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-22 11:10:14 +0300

    Time: 1600 ms (6.38%), Space: 234.9 MB (10.94%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -1,35 +1,27 @@
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        #multi source bfs
+        row, col = len(isWater), len(isWater[0])
+        direction = [(1,0),(-1,0),(0,1),(0,-1)]
         q = deque()
         visited = set()
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
-        row , column = len(isWater), len(isWater[0])
-
-        def inbound(r,c):
-            return r >= 0 and r < row and c >= 0 and c < column
-
+        def inBound(r,c):
+            return r>= 0 and c >= 0 and r < row and c < col
         for i in range(row):
-            for j in range(column):
-                if isWater[i][j] == 1:
+            for j in range(col):
+                val = isWater[i][j]
+                if val == 1:
+                    isWater[i][j] = 0
                     q.append((i,j))
                     visited.add((i,j))
-                    isWater[i][j] = 0
                 else:
                     isWater[i][j] = 1
-        
         while q:
             r, c = q.popleft()
-            for x,y in directions:
-                new_r = x + r
-                new_c = y + c
-                if inbound(new_r,new_c) and (new_r,new_c) not in visited:
-                    if isWater[r][c] == 0:
-                        isWater[new_r][new_c] = 1
-                    else:
-                        isWater[new_r][new_c] = isWater[r][c] +1
-
-                    visited.add((new_r,new_c))
-                    q.append((new_r,new_c))
+            for x, y in direction:
+                new_x = x + r
+                new_y = y + c
+                if inBound(new_x,new_y) and (new_x,new_y) not in visited:
+                    q.append((new_x,new_y))
+                    visited.add((new_x,new_y))
+                    isWater[new_x][new_y] += isWater[r][c]
         return isWater
-                    
