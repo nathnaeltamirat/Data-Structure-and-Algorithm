5fa0bafff100031e53af023bf78491e214a42315
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-23 20:06:10 +0300

    Time: 71 ms (52.27%), Space: 19.9 MB (11.3%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        valid = False
-        l = 0
         t_count = Counter(t)
         s_count = defaultdict(int)
-        res = float('inf')
+
+        start = end = 0
         def checker():
-            for i in t_count:
-                # print(t_count[i],s_count[i])
-                if t_count[i] > s_count[i]:
+            for item in t_count:
+                if t_count[item] > s_count[item]:
                     return False
             return True
-        start = end = 0
+        valid = False
+        l = 0
+        res = float('inf')
         for r in range(len(s)):
             s_count[s[r]] += 1
-            # print(s_count,t_count)
             if not valid:
                 if checker():
                     valid = True
             while valid and s_count[s[l]] > t_count[s[l]]:
-                s_count[s[l]] -= 1
-                l += 1
-            if valid:
-                if r -l + 1 < res:
-                    res = r-l+1
-
-                    start , end = l, r
-        return s[start:end+1] if res != float('inf') else ""
+                s_count[s[l]]-=1
+                l+=1
             
+            if valid and (r-l+1) < res:
+                res = r-l+1
+                start = l
+                end = r
+        return s[start:end+1] if res != float('inf') else ""
