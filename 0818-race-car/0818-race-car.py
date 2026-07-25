776fc829b05ed7d4c7e810fd8baa3bbff7039f60
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-25 10:54:17 +0300

    Time: 2812 ms (6.47%), Space: 329.2 MB (9.44%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,31 +1,33 @@
 class Solution:
     def racecar(self, target: int) -> int:
-        fuel = [0,1]
-
-        #count pos speed
-        q = deque([(0,0,1),])
-        visited = set([(0,1)])
-
-        def calc(choice,pos,speed):
-            if choice == 0:
-                if speed > 0:
-                    speed = - 1
-                    return (pos,speed)
-                return (pos,1)
-            else:
-                pos += speed
-                speed *= 2
-                return (pos,speed)
-        while q:
-            count, pos, speed = q.popleft()
-            # print(count,pos,speed)
-            for i in fuel:
-                new_pos, new_speed = calc(i,pos,speed)
-                if new_pos == target:
-                    return count + 1
-                if (new_pos,new_speed) not in visited:
-                    q.append((count +1,new_pos,new_speed))
-                    visited.add((new_pos,new_speed))
-
-
+        items = ['a','r']
+        #storing speed and position
+        q = deque([(1,0)])
+        visited = set([1,0])
+        res = 0
         
+        def speedAndPos(speed,pos,item):
+            if item == 'a':
+                pos += speed
+                speed *=2
+            else:
+                if speed > 0:
+                    speed = -1
+                else:
+                    speed = 1
+            return [speed,pos]
+
+
+        while True:
+            n = len(q)
+            res += 1
+            for _ in range(n):
+                speed,pos = q.popleft()
+                for item in items:
+                    new_speed, new_pos = speedAndPos(speed,pos,item)
+                    if (new_speed, new_pos) not in visited:
+                        visited.add((new_speed,new_pos))
+                        q.append((new_speed,new_pos))
+                        if new_pos == target:
+                            return res
+   
