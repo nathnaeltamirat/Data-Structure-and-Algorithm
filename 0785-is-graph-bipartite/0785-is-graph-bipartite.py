a2728678cca6ad96f65e45de277c92e75ce235c0
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-26 15:02:30 +0300

    Time: 0 ms (100%), Space: 20.7 MB (10.91%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -7,14 +7,18 @@ class Solution:
             for neigh in graph[node]:
                 if color[neigh] == -1:
                     color[neigh] = 1 - color[node]
-                    dfs(neigh)
+                    if not dfs(neigh):
+                        return False
                 elif color[neigh] == color[node]:
                     ans = False
                     return False
+            return True
         for node in range(len(graph)):
             if color[node] == -1:
                 color[node] = 0
-                dfs(node)
+                if not dfs(node):
+                    return False
+        return True
             
           
         
@@ -23,4 +27,3 @@ class Solution:
         return ans
             
             
-
