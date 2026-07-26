6e5357e74d0770500c3daa3683e47ec4422f9731
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-26 22:07:55 +0300

    Time: 0 ms (100%), Space: 19.4 MB (94.55%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,34 +1,34 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(list)
-        all_elements = set()
-        for i in range(len(equations)):
-            a, b = equations[i]
-            value = values[i]
-            graph[a].append([b,value])
-            graph[b].append([a,1/value])
-            all_elements.add(a)
-            all_elements.add(b)
-        res = []
-        def dfs(source,target):
-            visited.add(source)
-            if source == target:
-                return 1
-            for neigh,c_val in graph[source]:
-                if neigh not in visited:
-                    val = dfs(neigh,target)
-                    if val != -1:
-                        return val * c_val
-            return -1
+        variables = set()
         visited = set()
+        for i in range(len(equations)):
+            variables.add(equations[i][0])
+            variables.add(equations[i][1])
+            graph[equations[i][0]].append([equations[i][1],values[i]])
+            graph[equations[i][1]].append([equations[i][0],1/values[i]])
+       
+        res = 0
+        def dfs(src,dest):
+            visited.add(src)
+            if src == dest:
+                return 1
+            for neigh, val in graph[src]:
+                if neigh not in visited:
+                    value = val * dfs(neigh,dest)
+                    if value:
+                        return value
+            return 0
+        ans = []
         for a, b in queries:
-            if a not in all_elements or b not in all_elements:
-                res.append(-1)
+            if a not in variables or b not in variables:
+                ans.append(-1)
             else:
                 visited = set()
                 val = dfs(a,b)
-                if val:
-                    res.append(val)
+                if val == 0:
+                    ans.append(-1)
                 else:
-                    res.append(-1)
-        return res
+                    ans.append(val)
+        return ans
