8b76bd0ff98ae6e717655e96984dd9495bde6144
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-26 16:35:53 +0300

    Time: 3 ms (86.36%), Space: 21.1 MB (38.62%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #cycle detection  can be done using topsort i think but 
+        #lets do cycle detection
         graph = defaultdict(list)
+        color = [-1] * numCourses
         for a,b in prerequisites:
             graph[b].append(a)
-        def dfs(i):
-            temp = True
-            ans[i] = 0
-            for neigh in graph[i]:
-                if ans[neigh] == 0:
+        
+        def dfs(node):
+            color[node] = 0
+            for neigh in graph[node]:
+                if color[neigh] == 0:
                     return False
-                if ans[neigh] == -1:
-                    temp = temp and dfs(neigh)
-            ans[i] = 1
-            return temp
-        result = True
-        ans = [-1] * numCourses
+                if color[neigh] != 1 and not dfs(neigh):
+                    return False
+            color[node] = 1
+            return True
+        res = True
         for i in range(numCourses):
-            if ans[i] == -1:
-                result = result and dfs(i)
-        print(graph)
-        return result
+            if color[i] == -1:
+                res = res and dfs(i)
+                if not res:
+                    return False
+        return True        
