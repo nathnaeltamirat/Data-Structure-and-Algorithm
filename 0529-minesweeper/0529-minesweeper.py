4fa2b1fb079e5a0b0546c81c86a39cdab02ea02f
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-07-27 11:30:15 +0300

    Time: 7 ms (50.29%), Space: 23.1 MB (8.73%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,46 +1,32 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
-        
         row, col = len(board), len(board[0])
         def inBound(r,c):
-            return r >= 0 and c >= 0 and r < row and c < col
-    
-        direction = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-        def bombCalculator(i,j):
-            count = 0
-            for x,y in direction:
-                new_x = x + i
-                new_y = y + j
-                if inBound(new_x,new_y):
-                    if board[new_x][new_y] == "M":
-                        count += 1
-            return count
-    
+            return r>= 0 and r < row and c >= 0 and c < col
+        direction = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
         def dfs(i,j):
             if board[i][j] == "M":
                 board[i][j] = "X"
                 return
-            
-            if board[i][j] == "E":
-                val = bombCalculator(i,j)
-                if val:
-                    board[i][j] = str(val)
-                    return
-                else:
-                    board[i][j] = "B"
-            for x,y in direction:
-                new_x = i + x
+            board[i][j] = "B"
+            for x, y in direction:
+                new_x = x + i
                 new_y = y + j
                 if inBound(new_x,new_y):
-                    if board[new_x][new_y] == "E":
-                        val = bombCalculator(new_x,new_y)
-                        if val:
-                            board[new_x][new_y] = str(val)
-                        else:
-                            board[new_x][new_y] = "B"
+                    if board[new_x][new_y] == "M":
+                        board[i][j]  = 1 if board[i][j] == "B" else board[i][j] + 1
+            if board[i][j] == "B":
+                for x,y in direction:
+                    new_x = x + i
+                    new_y = y + j
+                    if inBound(new_x,new_y):
+                        if board[new_x][new_y] == "E":
                             dfs(new_x,new_y)
-    
-
         dfs(click[0],click[1])
-        return board
+        for i in range(row):
+            for j in range(col):
+                board[i][j] = str(board[i][j])
                     
+        
+        # print(board)
+        return board
