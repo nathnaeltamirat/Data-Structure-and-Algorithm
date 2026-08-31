d723aac4a8b4def57513376df04b96c699a6a02e
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-08-31 16:09:26 +0300

    Time: 172 ms (17.97%), Space: 46.1 MB (46.08%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -2,23 +2,20 @@ class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         graph = defaultdict(list)
         indegree = defaultdict(int)
+        q = deque()
         for a, b in relations:
             graph[a].append(b)
             indegree[b] += 1
-        q = deque()
         for i in range(1,n+1):
             if indegree[i] == 0:
                 q.append(i)
-        dp = [-1] * (n+1)
-
+        
+        dp = [0] * n
         while q:
             node = q.popleft()
-            curr_time = time[node-1]
-            dp[node] = max(dp[node],curr_time)
             for neigh in graph[node]:
-                neigh_time = time[neigh-1]
-                dp[neigh] = max(dp[neigh],neigh_time + dp[node])
-                indegree[neigh] -= 1
+                dp[neigh-1] = max(dp[neigh-1], dp[node-1] +  time[node-1] )
+                indegree[neigh] -=1
                 if indegree[neigh] == 0:
                     q.append(neigh)
-        return max(dp)
+        return max(dp[i] + time[i] for i in range(n))
