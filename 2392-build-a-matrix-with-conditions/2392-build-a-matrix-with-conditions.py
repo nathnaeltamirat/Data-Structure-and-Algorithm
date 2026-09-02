2c62e84c676f43202e8313ff7584c0361dcbdaa8
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-02 11:48:26 +0300

    Time: 15 ms (81.34%), Space: 26.7 MB (7.83%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -1,7 +1,7 @@
 class Solution:
     def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
-        def bfs(graph,indegree,length):
-            q = deque([item for item in length if indegree[item] == 0])
+        def topSort(nodes,graph,indegree):
+            q = deque([node for node in nodes if indegree[node] == 0])
             order = []
             while q:
                 node = q.popleft()
@@ -10,37 +10,37 @@ class Solution:
                     indegree[neigh] -= 1
                     if indegree[neigh] == 0:
                         q.append(neigh)
-            return order if len(order) == len(length) else []
+            return order if len(order) == len(nodes) else []
+        #initialization
+        mat = [[0] * k for _ in range(k)]
         row_graph = defaultdict(list)
         row_indegree = defaultdict(int)
-        for a,b in rowConditions:
-            row_graph[a].append(b)
-            row_indegree[b] += 1
-        row_order = bfs(row_graph,row_indegree,range(1,k+1))
-        if not row_order:
-            return []
         col_graph = defaultdict(list)
         col_indegree = defaultdict(int)
+        for a, b in rowConditions:
+            row_graph[a].append(b)
+            row_indegree[b] += 1
         for a,b in colConditions:
             col_graph[a].append(b)
             col_indegree[b] += 1
-        col_order = bfs(col_graph,col_indegree,range(1,k+1))
+        
+        row_order = topSort(range(1,k+1),row_graph,row_indegree)
+     
+        if not row_order:
+            return []
+        print(row_order)
+        col_order = topSort(range(1,k+1),col_graph,col_indegree)
         if not col_order:
             return []
-        # print(row_order)
-        # print(col_order)
-        res = [[0] * k for _ in range(k)]
-        print(res)
-        row_dict = defaultdict(int)
-        col_dict = defaultdict(int)
+        row_order1 = defaultdict(int)
+        col_order1 = defaultdict(int)
         for i in range(k):
-            row_dict[row_order[i]] = i
-            col_dict[col_order[i]] = i
-        # print(row_dict)
-        # print(col_dict)
+            row_order1[row_order[i]] = i
+            col_order1[col_order[i]] = i
         for i in range(1,k+1):
-            r = row_dict[i]
-            c = col_dict[i]
-            res[r][c] = i
-        return res
+            r = row_order1[i]
+            c = col_order1[i]
+            mat[r][c] = i
 
+        
+        return mat
