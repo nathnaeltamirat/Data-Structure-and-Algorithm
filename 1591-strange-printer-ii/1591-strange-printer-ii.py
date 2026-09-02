86e251f32c7020dfc454e98b527d5cd8acc3b7de
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-02 13:06:38 +0300

    Time: 157 ms (44.85%), Space: 19.7 MB (56.62%) - LeetHub

--- 1591-strange-printer-ii/1591-strange-printer-ii.py
+++ 1591-strange-printer-ii/1591-strange-printer-ii.py
@@ -1,42 +1,46 @@
 class Solution:
     def isPrintable(self, targetGrid: List[List[int]]) -> bool:
+        #initialize
+        graph = defaultdict(set)
+        indegree = defaultdict(int)
         holder = defaultdict(list)
         row, col = len(targetGrid), len(targetGrid[0])
-        all_elements = set()
+        elements = set()
+        q = deque()
+
+        #finding the square position
         for i in range(row):
             for j in range(col):
                 val = targetGrid[i][j]
-                all_elements.add(val)
+                elements.add(val)
                 if not holder[val]:
-                    holder[val] = [i,j,i,j]
+                    holder[val] = [i,j,i,j] #clock wise direction
                 else:
-                    holder[val][0] = min(holder[val][0],i)#top
-                    holder[val][1] = max(holder[val][1],j)#right
-                    holder[val][2] = max(holder[val][2],i)#bottom
-                    holder[val][3] = min(holder[val][3],j)#left
-        graph = defaultdict(list)
-        indegree = defaultdict(int)
-        for i in holder:
-            top,right,bottom,left = holder[i]
-            for j in range(top,bottom+1):
-                for k in range(left,right+1):
-                    val = targetGrid[j][k]
-                    if val != i:
-                        graph[i].append(val)
+                    holder[val][0] = min(holder[val][0],i) #Top
+                    holder[val][1] = max(holder[val][1],j) #right
+                    holder[val][2] = max(holder[val][2],i) #Bottom
+                    holder[val][3] = min(holder[val][3],j) #left
+            
+        
+        elements = list(elements)
+        for i in range(len(elements)):
+            top,right,bottom,left = holder[elements[i]]
+            original = elements[i]
+            for i in range(top,bottom+1):
+                for j in range(left,right+1):
+                    val = targetGrid[i][j]
+                    if val != original and val not in graph[original]:
+                        graph[original].add(val)
                         indegree[val] += 1
-        q = deque()
-        for item in list(all_elements):
-            if indegree[item] == 0:
-                q.append(item)
-        count = 0
+        for el in elements:
+            if indegree[el] == 0:
+                q.append(el)
+        checker = 0
         while q:
             node = q.popleft()
-            count += 1
-            for neigh in graph[node]:
+            checker += 1
+            for neigh in list(graph[node]):
                 indegree[neigh] -= 1
                 if indegree[neigh] == 0:
                     q.append(neigh)
-        return count == len(all_elements)
-
-             
-
+        return checker == len(elements)
