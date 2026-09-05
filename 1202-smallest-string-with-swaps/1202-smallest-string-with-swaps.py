40b340068219d53a4f20092d14a10195a726e475
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-05 14:12:41 +0300

    Time: 171 ms (14.91%), Space: 52.6 MB (37.37%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,19 +1,18 @@
+from collections import defaultdict
 class UnionFind:
     def __init__(self):
         self.parent = dict()
-        self.size = defaultdict(lambda: 1)
+        self.size = defaultdict(lambda:1)
     def find(self,x):
         if x not in self.parent:
             self.parent[x] = x
             return x
-        if self.parent[x] == x:
-            return x
-        self.parent[x] = self.find(self.parent[x])
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
     
     def union(self,x,y):
-        rootx = self.find(x)
-        rooty = self.find(y)
+        rootx, rooty = self.find(x), self.find(y)
         if rootx != rooty:
             if self.size[rootx] > self.size[rooty]:
                 self.parent[rooty] = rootx
@@ -21,21 +20,22 @@ class UnionFind:
             else:
                 self.parent[rootx] = rooty
                 self.size[rooty] += self.size[rootx]
+    def isConnected(self,x,y):
+        return self.find(x) == self.find(y)
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        sorter = defaultdict(list)
         dsu = UnionFind()
-
-        for i, (a,b) in enumerate(pairs):
+        holder = defaultdict(list)
+        for a,b in pairs:
             dsu.union(a,b)
         for i in range(len(s)):
-            root = dsu.find(i)
-            sorter[root].append(s[i])
-        for i in sorter:
-            sorter[i].sort(reverse = True)
+            holder[dsu.find(i)].append(s[i])
+        for item in holder:
+            holder[item].sort(reverse = True)
         res = []
         for i in range(len(s)):
-            root = dsu.find(i)
-            res.append(sorter[root].pop())
-        print(res)
-        return "".join(res)
+            value = holder[dsu.find(i)].pop()
+            res.append(value)
+        print(res)   
+        return "".join(res) 
+       
