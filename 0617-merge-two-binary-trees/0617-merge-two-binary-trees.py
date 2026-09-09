10b9ea1d325cb8f7321ad934f4a73decfd8bdf18
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-09 17:08:55 +0300

    Time: 0 ms (100%), Space: 19.5 MB (78.32%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -10,7 +10,7 @@ class Solution:
             return root2
         if not root2:
             return root1
-        nRoot = TreeNode(root1.val + root2.val)
-        nRoot.left = self.mergeTrees(root1.left,root2.left)
-        nRoot.right = self.mergeTrees(root1.right,root2.right)
-        return nRoot
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left,root2.left)
+        root1.right = self.mergeTrees(root1.right,root2.right)
+        return root1
