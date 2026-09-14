ce1f66b25745a2a834799367263315d338bc41b4
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-14 11:58:58 +0300

    Time: 41 ms (0.33%), Space: 19.7 MB (12.71%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,34 +1,37 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(list)
-        variables = set()
+        items = set()
         visited = set()
-        for i in range(len(equations)):
-            variables.add(equations[i][0])
-            variables.add(equations[i][1])
-            graph[equations[i][0]].append([equations[i][1],values[i]])
-            graph[equations[i][1]].append([equations[i][0],1/values[i]])
-       
-        res = 0
-        def dfs(src,dest):
-            visited.add(src)
-            if src == dest:
-                return 1
-            for neigh, val in graph[src]:
+        graph = defaultdict(list)
+        for i in range(len(values)):
+            val = values[i]
+            a, b =  equations[i]
+            items.add(a)
+            items.add(b)
+            graph[a].append((b,val))
+            graph[b].append((a,1/val))
+
+        def dfs(start,dest):
+            res = 1
+            if start == dest:
+                return res
+            for neigh,val in graph[start]:
                 if neigh not in visited:
-                    value = val * dfs(neigh,dest)
+                    visited.add(neigh)
+                    value = dfs(neigh,dest)
+                   
                     if value:
-                        return value
+                        return res * val * value
             return 0
-        ans = []
-        for a, b in queries:
-            if a not in variables or b not in variables:
-                ans.append(-1)
+        res = []
+        for start, dest in queries:
+            visited = set()
+            if start not in items or dest not in items:
+                res.append(-1)
             else:
-                visited = set()
-                val = dfs(a,b)
-                if val == 0:
-                    ans.append(-1)
+                val = dfs(start,dest)
+                if val:
+                    res.append(val)
                 else:
-                    ans.append(val)
-        return ans
+                    res.append(-1)
+        return res
