2dfa61732764864fe63ae4c12c9daf133e14245c
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-14 12:40:13 +0300

    Time: 1749 ms (11.36%), Space: 20.9 MB (42.74%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,19 +1,19 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         visited = set()
-       
-        def dfs(i):
-            visited.add(i)
-            count = 1
-            x, y,r = bombs[i]
-            for j in range(len(bombs)):
-                if i != j and  j not in visited:
-                    n_x, n_y, _ = bombs[j]
-                    if((x-n_x)**2 + (y-n_y)**2 <=r*r):
-                        count += dfs(j)
-            
-            return count
         res = 1
+        def dfs(i):
+            x,y,r = bombs[i]
+            visited.add(i)
+            res = 1
+            for j in range(len(bombs)):
+                if j != i and j not in visited:
+                    new_x,new_y, _ = bombs[j]
+                    
+                    if( (x-new_x)**2 + (y-new_y)**2 <= r*r):
+
+                        res += dfs(j)
+            return res
         for i in range(len(bombs)):
             visited = set()
             res = max(res,dfs(i))
