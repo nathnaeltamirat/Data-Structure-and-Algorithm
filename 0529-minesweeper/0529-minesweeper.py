2a27b51b1ff6b343444a4fe7b44199e0b9d5aa6d
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-15 10:36:23 +0300

    Time: 7 ms (47.46%), Space: 22.8 MB (35.77%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,32 +1,30 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         row, col = len(board), len(board[0])
+        direction = [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(-1,1),(1,1),(1,-1)]
         def inBound(r,c):
-            return r>= 0 and r < row and c >= 0 and c < col
-        direction = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
+            return r >= 0 and r < row and c >= 0 and c < col
+        
         def dfs(i,j):
             if board[i][j] == "M":
                 board[i][j] = "X"
                 return
-            board[i][j] = "B"
+            count = 0
             for x, y in direction:
-                new_x = x + i
-                new_y = y + j
+                new_x, new_y = x + i, y + j
                 if inBound(new_x,new_y):
                     if board[new_x][new_y] == "M":
-                        board[i][j]  = 1 if board[i][j] == "B" else board[i][j] + 1
-            if board[i][j] == "B":
-                for x,y in direction:
-                    new_x = x + i
-                    new_y = y + j
+                        count += 1
+                
+            if count:
+                board[i][j] = str(count)
+            else:
+                board[i][j] = "B"
+                for x, y in direction:
+                    new_x, new_y = x + i, y + j
                     if inBound(new_x,new_y):
                         if board[new_x][new_y] == "E":
                             dfs(new_x,new_y)
+                            
         dfs(click[0],click[1])
-        for i in range(row):
-            for j in range(col):
-                board[i][j] = str(board[i][j])
-                    
-        
-        # print(board)
         return board
