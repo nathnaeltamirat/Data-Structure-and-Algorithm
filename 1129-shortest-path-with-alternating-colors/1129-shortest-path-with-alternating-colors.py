d9befa7589e5ce2438426792608721d62bffd161
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-17 12:06:28 +0300

    Time: 1 ms (79.01%), Space: 19.4 MB (90.01%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,19 +1,18 @@
 class Solution:
-    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+    def shortestAlternatingPaths(self, n: int, redEdges: list[list[int]], blueEdges: list[list[int]]) -> list[int]:
         ans = [-1] * n
         graph = [[[],[]] for _ in range(n)]
+        for a,b in redEdges:
+            graph[a][0].append(b)
+        for a, b in blueEdges:
+            graph[a][1].append(b)
         q = deque([[0,0],[0,1]])
         visited = set(((0,0),(0,1)))
-        for a, b in redEdges:
-            graph[a][0].append(b)
-        for a,b in blueEdges:
-            graph[a][1].append(b)
-        
         dist = 0
         while q:
             n = len(q)
             for _ in range(n):
-                node, color = q.popleft()
+                node , color = q.popleft()
                 if ans[node] == -1:
                     ans[node] = dist
                 alt = 1 - color
@@ -21,5 +20,6 @@ class Solution:
                     if (neigh,alt) not in visited:
                         q.append((neigh,alt))
                         visited.add((neigh,alt))
+
             dist += 1
         return ans
