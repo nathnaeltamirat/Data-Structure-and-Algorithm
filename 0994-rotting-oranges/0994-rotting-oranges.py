bf6689be277c1a895bde3628963fefacc3897155
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2026-09-17 11:40:24 +0300

    Time: 7 ms (16.15%), Space: 19.2 MB (79.9%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,35 +1,37 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        direction = [(1,0),(0,1),(-1,0),(0,-1)]
-        row , col = len(grid), len(grid[0])
-        def inBound(r,c):
-            return r >= 0 and c>= 0 and r < row and c < col
-        fresh_orange  = 0
-        q = deque()
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        fresh_orange = 0
+        row, col = len(grid), len(grid[0])
+        direction = [(1,0),(0,1),(0,-1),(-1,0)]
         res = 0
+        q = deque()
+        def inBound(r,c):
+            return r>=0 and c >= 0 and r < row and c <col
         for i in range(row):
             for j in range(col):
                 val = grid[i][j]
                 if val == 2:
                     q.append((i,j))
                 elif val == 1:
-                    fresh_orange +=1
+                    fresh_orange += 1
         if fresh_orange == 0:
             return 0
-
- 
         while q:
             n = len(q)
-            print(q)
             for _ in range(n):
-                r, c = q.popleft()
+                i, j = q.popleft()
                 for x, y in direction:
-                    new_x = x + r
-                    new_y = y + c
-                    if inBound(new_x,new_y) and grid[new_x][new_y] == 1:
-                        grid[new_x][new_y] = 2
-                        q.append((new_x,new_y))
-                        fresh_orange -= 1
+                    new_x = i + x
+                    new_y = j + y
+                    if inBound(new_x,new_y):
+                        if grid[new_x][new_y] == 1:
+                            fresh_orange -= 1
+
+                            grid[new_x][new_y] = 2
+                            q.append((new_x,new_y))
             res += 1
+            if fresh_orange == 0:
+                return res
             
-        return res-1 if fresh_orange == 0 else -1
+        return -1
+
